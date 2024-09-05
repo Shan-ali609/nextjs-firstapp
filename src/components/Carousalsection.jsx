e095c13c962eba7dfc29a9a734651c9f951f3e05
Shan Ali <shan.softwareengineer007@gmail.com>
2024-09-04 18:11:09 -0700

    entity error fixed

--- src/components/Carousalsection.jsx
+++ src/components/Carousalsection.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import "../style/carousal.css";
+import Image from "next/image";
+
 export default function Carousalsection() {
   return (
     <div className="carousal-bgimage">
       <h1 className="carousal-heading"> What Our Customers Say</h1>
       <p className="description">
-    nec congue arcu lorem in nisl. Duis neque
-        lacus, viverra non mauris ac, pharetra rhoncus libero. Aliquam varius
-        viverra ex, in venenatis magna ornare sit amet. Integer varius sit amet
-        turpis eu ullamcorper.
+        nec congue arcu lorem in nisl. Duis neque lacus, viverra non mauris ac,
+        pharetra rhoncus libero. Aliquam varius viverra ex, in venenatis magna
+        ornare sit amet. Integer varius sit amet turpis eu ullamcorper.
       </p>
 
-      <img className="carousal-profilr" src="/images/jhon.jpg" alt="profile" />
+      <Image
+        width={400}
+        height={400}
+        className="carousal-profilr"
+        src="/images/jhon.jpg"
+        alt="profile"
+      />
 
       <h3>Jhon Doe</h3>
     </div>
--- src/components/Forestimagesection.jsx
+++ src/components/Forestimagesection.jsx
@@ -1,26 +1,20 @@
-import React from 'react'
-import "../style/forestimagesection.css"
+import React from "react";
+import "../style/forestimagesection.css";
 export default function Forestimagesection() {
   return (
-
     <>
-    <div>
-      <div className='forest'>
-        <img src='/images/forest.jpg' alt='forest-image'/>
-      </div>
-    </div>
-   
       <div>
-
-       <div className='projects'>
-        <h3>Our Latest Projects</h3>
-        <p>Here is our latest projects. You'll love them!</p>
-       </div>
-
-
+        <div className="forest">
+          <img src="/images/forest.jpg" alt="forest-image" />
+        </div>
       </div>
 
-
+      <div>
+        <div className="projects">
+          <h3>Our Latest Projects</h3>
+          <p>Here is our latest projects. You&apos;ll love them!</p>
+        </div>
+      </div>
 
       {/* <div>
         
@@ -46,28 +40,18 @@ export default function Forestimagesection() {
 
       </div> */}
 
-
-  <div class="gallery-grid">
-    <img src="/images/2.jpg" alt="gallery"/>
-    <img src="/images/1.jpg" alt="gallery"/>
-    <img src="/images/3.jpg" alt="gallery"/>
-    <img src="/images/4.jpg" alt="gallery"/>
-    <img src="/images/5.jpg" alt="gallery"/>
-    <img src="/images/6.jpg" alt="gallery"/>
-    <img src="/images/7.jpg" alt="gallery"/>
-    <img src="/images/8.jpg" alt="gallery"/>
-    <img src="/images/9.jpg" alt="gallery"/>
-    <img src="/images/10.jpg" alt="gallery"/>
-   
-
-   
-  </div>
-  
- 
-
-
+      <div class="gallery-grid">
+        <img src="/images/2.jpg" alt="gallery" />
+        <img src="/images/1.jpg" alt="gallery" />
+        <img src="/images/3.jpg" alt="gallery" />
+        <img src="/images/4.jpg" alt="gallery" />
+        <img src="/images/5.jpg" alt="gallery" />
+        <img src="/images/6.jpg" alt="gallery" />
+        <img src="/images/7.jpg" alt="gallery" />
+        <img src="/images/8.jpg" alt="gallery" />
+        <img src="/images/9.jpg" alt="gallery" />
+        <img src="/images/10.jpg" alt="gallery" />
+      </div>
     </>
-
-
-  )
+  );
 }
