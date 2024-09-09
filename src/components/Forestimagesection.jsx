eae2a8cd61d34dbf67aeae3804c0a6f8b3dbb65a
Shan Ali <shan.softwareengineer007@gmail.com>
2024-09-08 18:00:03 -0700

    some changings

--- src/components/Forestimagesection.jsx
+++ src/components/Forestimagesection.jsx
@@ -18,18 +18,6 @@ export default function Forestimagesection() {
 
     
 
-      {/* <div class="gallery-grid">
-        <img src="/images/2.jpg" alt="gallery" />
-        <img src="/images/1.jpg" alt="gallery" />
-        <img src="/images/3.jpg" alt="gallery" />
-        <img src="/images/4.jpg" alt="gallery" />
-        <img src="/images/5.jpg" alt="gallery" />
-        <img src="/images/6.jpg" alt="gallery" />
-        <img src="/images/7.jpg" alt="gallery" />
-        <img src="/images/8.jpg" alt="gallery" />
-        <img src="/images/9.jpg" alt="gallery" />
-        <img src="/images/10.jpg" alt="gallery" />
-      </div> */}
     </>
   );
 }
