bc15c41924f4a6496e147bb8f3caf677e8c088eb
Shan Ali <shan.softwareengineer007@gmail.com>
2024-09-12 17:18:31 -0700

    good

--- src/components/Mainclient.jsx
+++ src/components/Mainclient.jsx
@@ -7,10 +7,22 @@ export default function Mainclient() {
     <div className='main-clients'>
       <h1 className='main-clients-heading'>Our Main Clients</h1>
       <div className='flex-box'>
-        <Image src='/images/css-3.png' alt='CSS3' />
-        <Image src='/images/html-5.png' alt='HTML5' />
-        <Image src='/images/js.png' alt='JavaScript' />
-        <Image src='/images/react.png' alt='React' />
+        <Image src='/images/css-3.png'
+         height={300}
+         width={390}
+        alt='CSS3' />
+        <Image src='/images/html-5.png' 
+           height={300}
+           width={390}
+        alt='HTML5' />
+        <Image src='/images/js.png'
+           height={300}
+           width={390}
+        alt='JavaScript' />
+        <Image src='/images/react.png'
+           height={300}
+           width={390}
+        alt='React' />
       </div>
     </div>
   );
